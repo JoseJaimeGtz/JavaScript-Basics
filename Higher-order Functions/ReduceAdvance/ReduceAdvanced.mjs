4d7c86f73ab74a521a2aa9abe09cdfc212278132
Jose Jaime Gtz <jose_jaime@live.com.mx>
2021-08-06 18:04:48 -0500

    reduceAdvance exercise complete

---
+++ Higher-order Functions/ReduceAdvance/ReduceAdvanced.mjs
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs'
+
+// You need to execute 'node --experimental-modules ReduceAdvanced.mjs' the first time to run this script. After that, you can use only 'node ReduceAdvanced.mjs'
+
+// TODO: make an JSON print like the example below using reduce function
+// Take data from data.txt
+
+const output = readFileSync('data.txt', 'utf-8')
+    .trim() // remove any line breaks or spaces at the end of the file.
+    .split('\n') // The split() method divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array. The ('\n') separator delete all the \n characters from the string/file and create a new array element.
+    .map(line => line.split('\t')) // return an array for each line converting an element for each \t character.
+    .reduce((costumers, line) => { // reduce receives two arguments, "constumers" is the objet that we're constructing. The second argument is the line that we're iterating.
+        costumers[line[0]] = costumers[line[0]] || [];
+        costumers[line[0]].push({
+            name: line[1],
+            price: line[2],
+            quantity: line[3]
+        });
+        return costumers
+    }
+    , {}) // {} means that return an object.
+
+console.log(JSON.stringify(output, null, 2));
