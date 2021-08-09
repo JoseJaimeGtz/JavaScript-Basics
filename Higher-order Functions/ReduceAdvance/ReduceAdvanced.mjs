47fe3548a4a7ca2e5ef898b36c24a0b118e7c9ad
Jose Jaime Gtz <jose_jaime@live.com.mx>
2021-08-09 09:58:57 -0500

    Comments added in ReduceAdvanced file

--- Higher-order Functions/ReduceAdvance/ReduceAdvanced.mjs
+++ Higher-order Functions/ReduceAdvance/ReduceAdvanced.mjs
@@ -10,13 +10,13 @@ const output = readFileSync('data.txt', 'utf-8')
     .split('\n') // The split() method divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array. The ('\n') separator delete all the \n characters from the string/file and create a new array element.
     .map(line => line.split('\t')) // return an array for each line converting an element for each \t character.
     .reduce((costumers, line) => { // reduce receives two arguments, "constumers" is the objet that we're constructing. The second argument is the line that we're iterating.
-        costumers[line[0]] = costumers[line[0]] || [];
-        costumers[line[0]].push({
-            name: line[1],
+        costumers[line[0]] = costumers[line[0]] || []; // takes the array with the elements and added the next one without rewrite the element.
+        costumers[line[0]].push({ // adds the properties with values in each iteration in the array.
+            name: line[1], // assign values to the properties
             price: line[2],
             quantity: line[3]
         });
-        return costumers
+        return costumers // returns the entire object
     }
     , {}) // {} means that return an object.
 
